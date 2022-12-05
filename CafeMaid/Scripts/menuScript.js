ca979e499ab94ca766085cac081f6cdb0c7f2afd
AbdurrahmanKAYAHAN0 <abdurrahmankayahan0@gmail.com>
2022-12-05 23:27:18 +0300

    ürünleri çekme ve sepete ekleme sql sorguları ve diğer kodları yazıldı

--- CafeMaid/Scripts/menuScript.js
+++ CafeMaid/Scripts/menuScript.js
@@ -1,5 +1,38 @@
-﻿
-function UrunList() {
+﻿function UrunListAll() {  
+            $.ajax({
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                //headers: {
+                //    "__RequestVerificationToken": getTextValue("tokenInput")
+                //},
+                url: ("Menu.aspx/GetUrunListAll"),
+                data: JSON.stringify({ str: "AJAX TEST => " }),
+                dataType: "json",
+                success: function (data) {
+
+
+                    console.log(data);
+
+                    //document.getElementById("footerDiv").innerHTML += generateItem(item.id,item.adi);
+                    document.getElementById("Menu-Content").innerHTML = "";
+
+                    document.getElementById("Menu-Content").insertAdjacentHTML("afterbegin", generateItem(data))
+
+
+
+                },
+                error: function (req, status, error) {
+                    console.log(error)
+                }
+            });
+
+
+}
+
+
+
+
+function UrunList(id) {
     $.ajax({
         type: "POST",
         contentType: "application/json; charset=utf-8",
@@ -7,19 +40,18 @@ function UrunList() {
         //    "__RequestVerificationToken": getTextValue("tokenInput")
         //},
         url: ("Menu.aspx/GetUrunList"),
-        data: JSON.stringify({ str: "AJAX TEST => " }),
+        data: JSON.stringify({ kategoriId: id }),
         dataType: "json",
         success: function (data) {
 
       
             console.log(data);
-            for (var i in data.d) {
-                var item = data.d[i];
+          
+            //document.getElementById("footerDiv").innerHTML += generateItem(item.id,item.adi);
+            document.getElementById("Menu-Content").innerHTML = "";
 
-                //document.getElementById("footerDiv").innerHTML += generateItem(item.id,item.adi);
-                document.getElementById("kategoriId" + item.KategoriId).insertAdjacentHTML("afterend", generateItem(item.KategoriId, item.UrunAdi))
+            document.getElementById("Menu-Content").insertAdjacentHTML("afterbegin", generateItem(data))
 
-            }
 
 
         },
@@ -29,13 +61,84 @@ function UrunList() {
     });
 }
 
-function generateItem(id, isim) {
 
-    return "<p id='str" + id + "' onclick='itemOnclick(" + id + ")'>" + isim + "</p>";
+function generateItem(data) {
+    var text = "";
+    text += "<div id='row' class='row' >"
+    for (var i in data.d) {
+        var item = data.d[i];
+        var syc = 0;
+
+
+  
+        var adet = 1;
+
+        text += "<div class='flip-card' style='margin-right: 5px; margin-left: 5px;'>" +
+            "<div class='flip-card-inner'>" +
+            "<div class='flip-card-front'>" +
+            "<img src='" + item.UrunImage + "'  style='width:300px;height:300px;'>" +
+            "</div>" +
+            "<div class='flip-card-back'>" +
+            "<h1>" + item.UrunAdi + "</h1>" +
+            "<p>" + item.UrunAciklama + "</p>" +
+            "<input  type='text' value='1' hint='Adet Giriniz.' id='adet" + item.Id + "'></input>" +
+            "</br>" +
+            "<button class='btn btn-warning' onClick='SepeteEkle(" + item.Id + ")'>Satın Al</button>" +
+                "</div>" +
+                "</div>" +
+                "</div>";
+          //  document.getElementById("row" + syc / 5).insertAdjacentHTML("afterbegin", text1);
+        syc++;
+
+    }
+   
+    text += "</div>";
+
+    return text;
+    //return "<p id='str" + id + "' onclick='itemOnclick(" + id + ")'>" + isim + "</p>";
+
+}
+function SepeteEkle(id) {
+
+
+
+   // alert(item.Id);
+    alert("ADET: " + document.getElementById("adet" + id).value);
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        //headers: {
+        //    "__RequestVerificationToken": getTextValue("tokenInput")
+        //},
+        url: ("Menu.aspx/SepeteUrunEkle"),
+        data: JSON.stringify({ id: id, adet: document.getElementById("adet" + id).value }),
+        dataType: "json",
+        success: function (data) {
+
+            alert(data);
+
+            console.log(data);
+            for (var i in data.d) {
+                var item = data.d[i];
+
+
+
+                //document.getElementById("footerDiv").innerHTML += generateItem(item.id,item.adi);
+                //document.getElementById("down_menu_item").insertAdjacentHTML("afterbegin", generateKategoriItem(item.Id, item.Value))
+
+            }
+
+
+        },
+        error: function (req, status, error) {
+            console.log(error)
+        }
+    });
+
 
 
 }
-
 
 
 function KategoriList() {
@@ -55,9 +158,9 @@ function KategoriList() {
             for (var i in data.d) {
                 var item = data.d[i];
 
-
+               
                 //document.getElementById("footerDiv").innerHTML += generateItem(item.id,item.adi);
-                document.getElementById("contentFiled").insertAdjacentHTML("afterbegin", generateKategoriItem(item.Id, item.Value))
+                document.getElementById("down_menu_item").insertAdjacentHTML("afterbegin", generateKategoriItem(item.Id, item.Value))
 
             }
 
@@ -86,12 +189,15 @@ function generateKategoriItem(id, isim) {
 
 
     return temp;*/
-    return "<p id='kategoriId"+id+"' onclick='itemOnclick(" + id + ")'>" + isim + "</p>";
-
+   
+    // return "<p id='kategoriId"+id+"' onclick='itemOnclick(" + id + ")'>" + isim + "</p>";
+    return "<a class='dropdown-item' onClick='itemOnclick(" + id + ")'>" + isim + "</a>";
 
 }
 function itemOnclick(id) {
-    alert(id)
+    //location.replace("/Menu.aspx");
+
+    UrunList(id);
 }
 function itemSil(id) {
     document.getElementById("str" + id).remove()
